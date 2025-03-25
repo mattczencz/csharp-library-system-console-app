248734bc45860726e4095dd9863b8b09270970d7
Matt Czencz <Mattczencz24@gmail.com>
2025-03-24 18:55:21 -0600

    Initial commit

---
+++ LibraryPlatform/Library.cs
@@ -0,0 +1,50 @@
+﻿namespace LibraryPlatform;
+
+public class Library
+{
+    public string[] Books { get; } = new string[5];
+
+    public void AddBook(string name)
+    {
+        int freeSlot = Array.FindIndex(Books, s => string.IsNullOrEmpty(s));
+
+        if (freeSlot < 0)
+        {
+            Console.WriteLine("Library is full. Please remove a book to add another.");
+            return;
+        }
+
+        Books[freeSlot] = name;
+    }
+
+    public void RemoveBook(string name)
+    {
+        int bookIndex = Array.FindIndex(Books, s => s == name);
+
+        if (bookIndex < 0)
+        {
+            Console.WriteLine("Could not find that book to remove.");
+            return;
+        }
+
+        Books[bookIndex] = "";
+    }
+
+    public void DisplayBooks()
+    {
+        string[] booksWithTitle = Books.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+        if (booksWithTitle.Length > 0)
+        {
+            Console.WriteLine("================ Books in your library ================");
+            foreach (var book in booksWithTitle)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        else
+        {
+            Console.WriteLine("You don't have any books in your library.");
+        }
+    }
+}
---
+++ LibraryPlatform/Program.cs
@@ -0,0 +1,50 @@
+﻿using LibraryPlatform;
+
+bool running = true;
+Library library = new Library();
+
+Console.WriteLine("Welcome to your virtual library!");
+
+do
+{
+    Console.WriteLine("Please enter the number of the action you would like to do.");
+    Console.WriteLine("1. Add Book");
+    Console.WriteLine("2. Remove Book");
+    Console.WriteLine("3. View Books");
+    Console.WriteLine("4. Exit");
+
+    string? choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            library.DisplayBooks();
+            break;
+        case "4":
+            Console.WriteLine("Exiting application.");
+            running = false;
+            break;
+        default:
+            Console.WriteLine("Invalid selection.");
+            break;
+    }
+} while (running);
+
+
+#region Program Actions
+
+void AddAction()
+{
+    
+}
+
+void RemoveAction()
+{
+    
+}
+
+#endregion
